5013f3c5bbeeed0c7175efb2f811fa6e585bce57
Strikeless <56199495+Strikeless@users.noreply.github.com>
2026-05-11 16:29:32 +0300

    Change build scripts to target a 26.1.x multiversion, instead of 26.1.2
    Additionally updated the fabric loader dependency to 0.19.2, the recommended version for 26.1, since 0.18 apparently wasn't ever a thing for 26.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,11 +20,11 @@ plugins {
 stonecutter {
     create(rootProject) {
         // Here we specify the target game versions that Stonecutter will try to build the mod for.
-        versions("26.1.2")
+        versions("26.1")
 
         // Specify an explicit target version for Stonecutter to reset to when running the "Reset active version" Gradle task.
         // The task should be run before committing changes, so that there's no diffs from Stonecutter's preprocessor internals.
-        vcsVersion = "26.1.2"
+        vcsVersion = "26.1"
     }
 }
 
